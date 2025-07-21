f30b454263180c7f54bc60d77681576d2cd8c162
smailyazidi <smailyazidi@gmail.com>
2025-07-21 14:54:37 +0100

    Updated personal info

--- lib/models/Tutor.ts
+++ lib/models/Tutor.ts
@@ -1,10 +1,14 @@
-// /lib/models/Tutor.ts
+
 import mongoose from "mongoose"
 
-const TutorSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  image: String,
-})
+const TutorSchema = new mongoose.Schema(
+  {
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    image: { type: String, default: null },
+  },
+  { timestamps: true }
+)
 
 export default mongoose.models.Tutor || mongoose.model("Tutor", TutorSchema)
+
